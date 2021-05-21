aea62d132a96faeef6c766ca244e0366c1b7ae71
shubham pandey <shubhampnd414@gmail.com>
2021-05-21 17:16:33 +0530

    new update

--- public/scripts/homefilter.js
+++ public/scripts/homefilter.js
@@ -75,7 +75,7 @@ let renderResult=(value)=>{
   let  blogsRow=document.querySelector("#blogs_row")
   let blogs=fetch(`/blogs/sort/${value}`,{method:"GET",headers:{"Content-Type":"application/json"}})
   let blog =`<div class="container-fluid">
-  <div class=" justify-content-around row" id="blogs_row"> `
+  <div class=" justify-content-start row" id="blogs_row"> `
 
  blogsRow.innerHTML='<strong class="p-2 text-primary">Loading ...</strong>'
 
@@ -90,7 +90,7 @@ let renderResult=(value)=>{
       data["blogs"].forEach(element => {
     
      blog+=`
-       <div class="card  mb-5 p-4  col-md-3">
+       <div class="card card-demo  mb-5 p-4  ">
   <div class="card-header text-center blog_title  font-weight-bold">${element.title}</div>
   <div class="card-body p-2">
       <input type="text" class="blog_id" hidden value="${element._id}">
